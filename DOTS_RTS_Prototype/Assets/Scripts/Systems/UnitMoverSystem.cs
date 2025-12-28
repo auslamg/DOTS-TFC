d45ee191901982344304b2b54a7aa1070a3ed4dd
Gab <ausejolamadridg@gmail.com>
2025-12-28 22:08:43 +0100

    UnitMoveSystem refactored into physics

--- DOTS_RTS_Prototype/Assets/Scripts/Systems/UnitMoverSystem.cs
+++ DOTS_RTS_Prototype/Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -1,6 +1,7 @@
 using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Physics;
 using Unity.Transforms;
 using UnityEngine;
 
@@ -11,18 +12,23 @@ partial struct UnitMoverSystem : ISystem
     {
         foreach ((
             RefRW<LocalTransform> localTransform,
-            RefRO<MoveSpeed> moveSpeed)
+            RefRO<MoveSpeed> moveSpeed,
+            RefRW<PhysicsVelocity> physicsVelocity)
                in SystemAPI.Query<
                 RefRW<LocalTransform>,
-                RefRO<MoveSpeed>>())
+                RefRO<MoveSpeed>,
+                RefRW<PhysicsVelocity>>())
         {
-            //Desired move direction based on target difference
+            //Desired normalized move direction based on target difference
             float3 targetPosition = localTransform.ValueRO.Position + new float3(10, 0, 0);
             float3 moveDirection = targetPosition - localTransform.ValueRO.Position;
             moveDirection= math.normalize(moveDirection);
 
-            localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());
-            localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.value * SystemAPI.Time.DeltaTime;
+            localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up()); //Unit rotation toward move direction
+
+            physicsVelocity.ValueRW.Linear = moveDirection * moveSpeed.ValueRO.value;
+            physicsVelocity.ValueRW.Angular = float3.zero;
+            //localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.value * SystemAPI.Time.DeltaTime;
         }
     }
 }
